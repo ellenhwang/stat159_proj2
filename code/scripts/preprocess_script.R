da45d49090757b66485abc411db2012d782e36d7
Joseph Simonian <j.simonian10@gmail.com>
2016-11-03 01:00:32 -0700

    Fix bug in preprocess script

--- code/scripts/preprocess_script.R
+++ code/scripts/preprocess_script.R
@@ -7,13 +7,13 @@ new_credit <- cbind(temp_credit[ ,-1], Balance = credit$Balance)
 # Mean Centering and Standardizing
 scaled_credit <- scale(new_credit, center = TRUE, scale = TRUE)[,-1]
 write.csv(scaled_credit, file = "data/Processed_Credit.csv")
-op
+
 # Create Train and Test Sets
 set.seed(1)
 samp <- sample(nrow(scaled_credit), 300)
 
 train <- scaled_credit[samp,][,-1]
 test <- scaled_credit[-samp,][,-1]
-  
+
 write.csv(train, file = 'data/train.csv')
 write.csv(test, file = 'data/test.csv')
